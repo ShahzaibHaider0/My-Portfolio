0b73d09912d311a1a88f94065152cb791084b8c7
Shahzaib Haider <150579950+ShahzaibHaider0@users.noreply.github.com>
2024-09-08 23:56:37 +0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "SHAHZAIB HAIDER",
   profile: '/profile.png',
-  designation: "Embedded Engineer",
+  designation: "Computer System Geek",
   description: "My name is Shahzaib Haider. I am an enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'malikshahzaibaps@gmail.com',
   phone: '+923260670664',
