35732584cfccfb66c7ff78e58496f50679a4011d
Shahzaib Haider <150579950+ShahzaibHaider0@users.noreply.github.com>
2024-09-09 15:15:23 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import { Helmet } from 'react-helmet';
-
+\\updated
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
