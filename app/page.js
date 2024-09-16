3e4e2bffacf9c95c18dc175c974700161ab0d076
Shahzaib Haider <150579950+ShahzaibHaider0@users.noreply.github.com>
2024-09-17 01:53:40 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 async function getData() {
+  const devUsername = "shahaibhaider"; 
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
   if (!res.ok) {
